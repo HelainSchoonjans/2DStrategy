7f2c249f3bd4b50251eb28a64804dff15ee28536
Hélain Schoonjans <helain90@hotmail.com>
2020-04-18 21:42:37 +0200

    Added tiles and script to pick their graphics randomly

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private SpriteRenderer renderer;
+    public Sprite[] tileGraphics;
+
+    private void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        int randomTile = Random.Range(0, tileGraphics.Length);
+        renderer.sprite = tileGraphics[randomTile];
+    }
+}
