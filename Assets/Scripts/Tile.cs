47806390526f55d06f490d12b295acf4e5b43e0a
Hélain Schoonjans <helain90@hotmail.com>
2020-04-19 08:37:09 +0200

    Added obstacle layer

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -7,10 +7,36 @@ public class Tile : MonoBehaviour
     private SpriteRenderer renderer;
     public Sprite[] tileGraphics;
 
+    public float hoverAmount;
+
+    public LayerMask obstacleLayer;
+
     private void Start()
     {
         renderer = GetComponent<SpriteRenderer>();
         int randomTile = Random.Range(0, tileGraphics.Length);
         renderer.sprite = tileGraphics[randomTile];
+    }
+
+    private void OnMouseEnter()
+    {
+        transform.localScale += Vector3.one * hoverAmount;
+    }
+
+    private void OnMouseExit()
+    {
+        transform.localScale -= Vector3.one * hoverAmount;
+    }
+
+    public bool IsClear()
+    {
+        Collider2D obstacle = Physics2D.OverlapCircle(transform.position, 0.2f, obstacleLayer);
+        if( obstacle != null)
+        {
+            return false;
+        } else
+        {
+            return true;
+        }
     }
 }
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ A game based on this [Udemy course](https://www.udemy.com/course/the-ultimate-gu
 
 ## Skipped steps
 - creating trees
+- setting the obstacle layer to obstacle (https://www.udemy.com/course/the-ultimate-guide-to-making-a-2d-strategy-game-in-unity/learn/lecture/16984222?components=buy_button%2Cdiscount_expiration%2Cgift_this_course%2Cpurchase%2Cdeal_badge%2Credeem_coupon#overview)
