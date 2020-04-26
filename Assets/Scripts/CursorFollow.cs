360024169262b6bb22094cc0cec4fc7301f63e0f
Hélain Schoonjans <helain90@hotmail.com>
2020-04-26 15:56:06 +0200

    The cursor now follows the mouse

---
+++ Assets/Scripts/CursorFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorFollow : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPosition;
+    }
+}
